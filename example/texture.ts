e5fb9d9d00d1eb369eb0c849138c7e2261038700
Rye Terrell <ryeterrell@ryeterrell.net>
2021-09-24 00:07:31 -0500

    update demo

--- example/texture.ts
+++ example/texture.ts
@@ -1,5 +1,5 @@
 const size = 512;
-const count = 16;
+const count = 255;
 
 export const textureData = new Uint8Array(size * size);
 textureData.fill(255);
@@ -8,13 +8,13 @@ const walkers: { color: number; x: number; y: number }[] = [];
 
 for (let i = 0; i < count; i++) {
   walkers.push({
-    color: 255 - (i + 1) * 8,
+    color: 255 - i,
     x: Math.floor(Math.random() * size),
     y: Math.floor(Math.random() * size),
   });
 }
 
-for (let step = 0; step < 100000; step++) {
+for (let step = 0; step < 20000; step++) {
   for (const walker of walkers) {
     if (Math.random() < 0.5) {
       walker.x += Math.random() < 0.5 ? 1 : -1;
